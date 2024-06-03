8da5c8027e3a3bc924ba7acad345d9dac5a23a7d
xHarryxGG <harrysalazar2do@gmail.com>
2024-06-02 23:38:29 -0400

    render

--- ProyectoBiblioteca/settings.py
+++ ProyectoBiblioteca/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'ProyectoBiblioteca.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost/postgres',
+        default='postgres://admin:777@localhost/postgres',
         conn_max_age=600
     )
 }
